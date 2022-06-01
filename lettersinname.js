1da59d7b55a0094cb9751bbb20a30792aac153a8
camronthecoder <106614872+camronthecoder@users.noreply.github.com>
2022-06-01 11:29:22 -0400

    first Commit

---
+++ lettersinname.js
@@ -0,0 +1,11 @@
+let fullName = "Cameorn Haley "
+let currentName = ""
+let lettersInName = 0 
+let bothNames = fullName.split(" ")
+
+for (let i = 0; i < bothNames.length; i++) {
+    for (let j=0; j<bothNames[i].length; j++){
+        lettersInName++
+    }
+console.log(bothNames)
+}
